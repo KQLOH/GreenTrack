3db19a82beb08698723051f17b21153df8755d7b
KQLOH <kqloh123@gmail.com>
2026-04-20 12:38:07 +0800

    update error 2.0

--- lib/User/dashboard_screen.dart
+++ lib/User/dashboard_screen.dart
@@ -1,6 +1,7 @@
 ﻿import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import '../services/supabase_client.dart';
+import 'recycle_screen.dart';
 
 final supabase = supabaseClient;
 
